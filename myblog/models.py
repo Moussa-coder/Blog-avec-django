f211f5d6e13079c301cb46b692701ccb25c61da4
Moussa-codeur <correamoussa469@gmail.com>
2025-05-24 22:43:04 +0200

    ESSAI

--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class CreateBlog(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200) # pour les urls
+    intro = models.TextField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='media',)
+    date_added = models.DateTimeField(auto_now_add=True)
+ 
+    class Meta:
+        ordering = ['-date_added'] # les plus recents en premier
+
+class Comment(models.Model):
+    post = models.ForeignKey(CreateBlog, related_name='comments', on_delete=models.CASCADE)
+    email = models.EmailField()
+    body = models.TextField()
+    name = models.CharField(max_length=100, default='inconnu')
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_added']
+
+    def __str__(self):
+        return f'{self.name} a commenté {self.post.title}'
+
